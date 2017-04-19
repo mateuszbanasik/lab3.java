979a5a4b2bf9c2336915a39e50c18f16febbf524
mateuszbanasik <mbanasik1994@gmail.com>
2017-04-19 19:29:37 +0200

    change

---
+++ Test.java
@@ -0,0 +1,24 @@
+public class Test {
+		public static void main(String[] args) {
+			Vector2D wektorr1=new Vector2D(2,3,4,5); //tworzymy wektor1 za pomoca konstruktora z parametrami
+			wektorr1.wysw();
+			Vector2D wektorr2=new Vector2D(7,8,9,10);//tworzymy wektor2 za pomoca konstruktora z parametrami
+			wektorr2.wysw();
+			Vector2D wektorr3=new Vector2D();
+			Vector2D wektorr4=new Vector2D();
+			Vector2D wektorr5=new Vector2D();
+			Vector2D wektorr6=new Vector2D();
+			wektorr3=wektorr1.sumaVector(wektorr2); //tworzymy nowy wektor(wektorr3) majacy wspolrzedne sumy wektorr1+wektorr2
+			wektorr3.wysw();
+			wektorr4=wektorr1.minusVector(wektorr2); //tworzymy nowy wektor(wektorr4) majacy wspolrzedne roznicy wektorr1-wektorr2
+			wektorr4.wysw();
+			wektorr5=wektorr1.mnozenieVector(5);//tworzymy wektor(wektorr5) maj¹cy wspó³rzêdne 5krotnie wiêksze od wektorr1
+			wektorr5.wysw();
+			wektorr6=wektorr1.normalVector(wektorr1);//tworzymy wektor(wektorr6) który jest znormalizowanym wektorrem1
+			wektorr6.wysw();
+			
+			
+			
+		}
+		
+}
---
+++ Vector2D.java
@@ -0,0 +1,58 @@
+public class Vector2D {
+	public double xp;//wspolrzedna poczatkowa x
+	public double yp;//wspolrzedna poczatkowa y
+	public double xk;//wspolrzedna koncowa x
+	public double yk;//wspolrzedna koncowa y
+	public double x;// wspolrzedna x wektora (xk-xp)
+	public double y;//wspolrzedna y wektora (yk-yp)
+	public double d; //dlugosc wektora
+	public Vector2D(){ // konstruktor domyslny
+		this.xp=0;
+		this.yp=0;
+		this.xk=0;
+		this.yk=0;
+		this.x=0;
+		this.y=0;
+		this.d=0;
+	}
+	public Vector2D(double xp,double xk,double yp,double yk){ //konstruktor z parametrami
+		this.xp=xp;
+		this.xk=xk;
+		this.yp=yp;
+		this.yk=yk;
+		
+	}
+	public Vector2D sumaVector(Vector2D wektor){ //suma wektora dla którego zosta³a wywo³ana metoda i wektora podanego jako parametr
+		Vector2D wektor1= new Vector2D(this.xp+wektor.xp,this.xk+wektor.xk,this.yp+wektor.yp,this.yk+wektor.yk)	;
+		return wektor1;
+	}
+	
+	public Vector2D minusVector(Vector2D wektor){ //roznica wektora dla którego zosta³a wywo³ana metoda i wektora podanego jako parametr.
+		Vector2D wektor2= new Vector2D(this.xp-wektor.xp,this.xk+wektor.xk,this.yp+wektor.yp,this.yk+wektor.yk)	;
+		return wektor2;
+	}
+	public Vector2D mnozenieVector(double s){ // metoda z parametrem, przemnozenie wsp. wektora przez stala
+		Vector2D wektor3=new Vector2D(s*this.xp,s*this.xk,s*this.yp,s*this.yk);
+		return wektor3;
+	}
+	public void Dlugosc(){ //metoda bez parametrow zwracajaca dlugosc wektora
+		this.x=this.xk-this.xp;
+		this.y=this.yk-this.yp;
+		this.d=Math.sqrt(Math.pow(x, 2)+(Math.pow(y, 2)));
+		System.out.println("dlugosc wynosi:"+this.d+"\n");
+		}
+	public double getDlugosc(){
+		return this.d;
+	}
+	public Vector2D normalVector(Vector2D wektor){ // normalizacja wektora
+		Vector2D wektor4=new Vector2D(this.xp/this.d,this.xk/this.d,this.yp/this.d,this.yk/this.d);
+		return wektor4;
+	}
+		public void wysw(){
+			System.out.println("Wektor");
+			System.out.println("wspolrzedna xp wynosi: "+this.xp+"\n"+"wspolrzedna wektora xk wynosi: "+this.xk);
+			System.out.println("wspolrzedna yp wynosi: "+this.yp+"\n"+"wspolrzedna yk wynosi: "+this.yk);
+			Dlugosc();
+		}
+		}
+
